1179a9c0dd67bd4053ddc88c0f9f535a6a3cfa55
Matt C <108201614+mattc-27@users.noreply.github.com>
2025-03-01 20:29:34 -0700

    updates 20250301001

--- auto-detail/scripts/services.js
+++ auto-detail/scripts/services.js
@@ -7,16 +7,19 @@ const serviceData = [
         "details": [
             {
                 "id": 1,
-                "description": "Exterior Wash & Dry: Thorough hand wash and drying to remove dirt and grime.",
+                "name": "Exterior Wash & Dry",
+                "description": "Thorough hand wash and drying to remove dirt and grime.",
 
             },
             {
                 "id": 2,
-                "description": "Wheel Cleaning: Quick cleaning of wheels and tires for a refreshed look."
+                "name": "Wheel Cleaning",
+                "description": "Quick cleaning of wheels and tires for a refreshed look."
             },
             {
                 "id": 3,
-                "description": "Interior Vacuuming: Light vacuuming of carpets and seats to remove surface debris."
+                "name": "Interior Vacuuming",
+                "description": "Light vacuuming of carpets and seats to remove surface debris."
             },
             /*       {
                 "id": 1,
@@ -33,15 +36,18 @@ const serviceData = [
         "details": [
             {
                 "id": 1,
-                "description": "Complete Interior Detailing: Deep cleaning of carpets, seats, and surfaces for a like-new appearance."
+                "name": "Complete Interior Detailing",
+                "description": "Deep cleaning of carpets, seats, and surfaces for a like-new appearance."
             },
             {
                 "id": 2,
-                "description": "Wax Application: Hand-applied wax for a lasting shine and protection."
+                "name": "Wax Application",
+                "description": "Hand-applied wax for a lasting shine and protection."
             },
             {
                 "id": 3,
-                "description": "Window & Mirror Cleaning: Streak-free cleaning for crystal-clear visibility."
+                "name": "Window & Mirror Cleaning",
+                "description": "Streak-free cleaning for crystal-clear visibility."
             }
         ],
         "image": "https://images.unsplash.com/photo-1589362086172-2c29bea70386?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -54,15 +60,18 @@ const serviceData = [
         "details": [
             {
                 "id": 1,
-                "description": "Full Exterior Detailing: Includes clay bar treatment, polish, and premium wax for ultimate shine and paint protection."
+                "name": "Full Exterior Detailing",
+                "description": "Includes clay bar treatment, polish, and premium wax for ultimate shine and paint protection."
             },
             {
                 "id": 2,
-                "description": "Comprehensive Interior Detailing: Shampooing of carpets and upholstery, leather conditioning, and odor neutralizing."
+                "name": "Comprehensive Interior Detailing",
+                "description": "Shampooing of carpets and upholstery, leather conditioning, and odor neutralizing."
             },
             {
                 "id": 3,
-                "description": "Engine Bay Cleaning: Professional cleaning and detailing of the engine bay to remove grease and grime."
+                "name": "Engine Bay Cleaning",
+                "description": "Professional cleaning and detailing of the engine bay to remove grease and grime."
             }
         ],
         "image": "https://images.unsplash.com/photo-1708805282785-77092a1628d3?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -75,27 +84,33 @@ $(document).ready(function () {
 
     serviceData.forEach(function (item) {
         var card = `
-            <div class='card__lg content__col content__even' data-id='${item.id}'>
-                <div class='card__details--services'>
-                    <div class="card__item--pricing">
-                        <p class="card__text--md">
-                            $${item.membershipPrice}/year
-                        </p>
-                        <p class="card__text--sm">
-                            $${item.singleWash} single wash
-                        </p>
+                <div class='pricing_card' data-id='${item.id}'>
+                    <div class='pricing_card-details'>
+                        <div class="pricing_card-name">
+                            <h2>${item.name}</h2>
+                        </div>
+
+                        <div class="pricing_card-pricing">
+                            <p class="pricing_card-text-md">
+                                $${item.membershipPrice}/year
+                            </p>
+                            <p class="pricing_card-text-sm">
+                                $${item.singleWash} single wash
+                            </p>
+                        </div>
+
                     </div>
-                    <div class="card__item--name">
-                        <h2>${item.name}</h2>
+                    <div class="pricing_card-col">
+                        <ul 
+                            class="pricing_card-ul" 
+                            id="details-${item.id}">
+                        </ul>
+                      
                     </div>
+                         <div class="pricing_card-row">
+                           <a type=button href="../membership/index.html" class='pricing_card-btn'>GET STARTED</a>
+                         </div
                 </div>
-                <div class="card__section">
-                    <ul class="ul__col" id="details-${item.id}">
-                    </ul>
-                    <button class='view__button button--solid--blue w80'>GET STARTED</button>
-                </div>
-                
-            </div>
             `;
 
         $('#service-content').append(card);
@@ -103,7 +118,7 @@ $(document).ready(function () {
         // Map over details and append to the corresponding ul
         if (item.details && item.details.length > 0) {
             item.details.forEach(function (detail) {
-                $(`#details-${item.id}`).append(`<li>${detail.description}</li>`);
+                $(`#details-${item.id}`).append(`<li><i class="fa-solid fa-check check_dark"></i> ${detail.description}</li>`);
             });
         }
     });
